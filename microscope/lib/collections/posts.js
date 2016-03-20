4940a04be31175e3c66c5c1bd0e1762fe0c1d75f
Ethan-N <ethannevidomsky@gmail.com>
2016-03-20 17:41:51 -0500

    Added comments

--- microscope/lib/collections/posts.js
+++ microscope/lib/collections/posts.js
@@ -45,6 +45,12 @@ userId: user._id,
 author: user.username,
 submitted: new Date()
 });
+var post = _.extend(postAttributes, {
+userId: user._id,
+author: user.username,
+submitted: new Date(),
+commentsCount: 0
+});
 var postId = Posts.insert(post);
 return {
 _id: postId
