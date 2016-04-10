da7a9b8d56fab020d214fe85e50cbba368d3c353
Ethan-N <ethannevidomsky@gmail.com>
2016-04-10 14:15:48 -0500

    Added Voting

--- microscope/server/fixtures.js
+++ microscope/server/fixtures.js
@@ -1,6 +1,7 @@
 // Fixture data
 if (Posts.find().count() === 0) {
 var now = new Date().getTime();
+
 // create two users
 var tomId = Meteor.users.insert({
 profile: { name: 'Tom Coleman' }
@@ -16,7 +17,9 @@ userId: sacha._id,
 author: sacha.profile.name,
 url: 'http://sachagreif.com/introducing-telescope/',
 submitted: new Date(now - 7 * 3600 * 1000),
-commentsCount: 2
+commentsCount: 2,
+upvoters: [],
+votes: 0
 });
 Comments.insert({
 postId: telescopeId,
@@ -38,7 +41,9 @@ userId: tom._id,
 author: tom.profile.name,
 url: 'http://meteor.com',
 submitted: new Date(now - 10 * 3600 * 1000),
-commentsCount: 0
+commentsCount: 0,
+upvoters: [],
+votes: 0
 });
 Posts.insert({
 title: 'The Meteor Book',
@@ -46,7 +51,9 @@ userId: tom._id,
 author: tom.profile.name,
 url: 'http://themeteorbook.com',
 submitted: new Date(now - 12 * 3600 * 1000),
-commentsCount: 0
+commentsCount: 0,
+upvoters: [],
+votes: 0
 });
 for (var i = 0; i < 10; i++) {
 Posts.insert({
@@ -54,8 +61,10 @@ title: 'Test post #' + i,
 author: sacha.profile.name,
 userId: sacha._id,
 url: 'http://google.com/?q=test-' + i,
-submitted: new Date(now - i * 3600 * 1000),
-commentsCount: 0
+submitted: new Date(now - i * 3600 * 1000 + 1),
+commentsCount: 0,
+upvoters: [],
+votes: 0
 });
 }
 }
